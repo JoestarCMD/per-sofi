a3a040998cd9c520d428b68223566de19e1ddc12
Fra <jojoestarsupremacy@gmail.com>
2026-07-17 05:04:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Per Sofi ❤️");
+const button = document.getElementById("startButton");
+
+
+button.addEventListener("click", () => {
+
+    document.body.classList.add("fade");
+
+
+});
