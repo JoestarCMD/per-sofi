b8ea5b172897dd9fa9bc20759ef0d702289f6a7f
Fra <jojoestarsupremacy@gmail.com>
2026-07-17 08:41:54 +0200

    Update lettera.js

--- lettera.js
+++ lettera.js
@@ -1,55 +1,5 @@
-<!DOCTYPE html>
-<html lang="it">
+const bottone = document.getElementById("openLetter");
 
-<head>
-
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<title>Per Sofi ❤️</title>
-
-<link rel="stylesheet" href="style.css">
-
-</head>
-
-
-<body>
-
-
-<div class="letter-page">
-
-
-<div class="envelope" id="envelope">
-
-    <div class="letter-icon">
-        ✉️
-    </div>
-
-    <h1>
-        Una lettera per te
-    </h1>
-
-    <button id="openLetter">
-        Apri ❤️
-    </button>
-
-</div>
-
-
-
-<div class="letter" id="letter">
-
-<p id="letterText"></p>
-
-</div>
-
-
-</div>
-
-
-<script src="script.js"></script>
-
-
-</body>
-
-</html>
+bottone.addEventListener("click", () => {
+    alert("FUNZIONAAAAA ❤️");
+});
