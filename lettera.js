35462c195d4713c2401ab240acfcf807c8b01a31
Fra <jojoestarsupremacy@gmail.com>
2026-07-17 08:32:07 +0200

    Create lettera.js

---
+++ lettera.js
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Per Sofi ❤️</title>
+
+<link rel="stylesheet" href="style.css">
+
+</head>
+
+
+<body>
+
+
+<div class="letter-page">
+
+
+<div class="envelope" id="envelope">
+
+    <div class="letter-icon">
+        ✉️
+    </div>
+
+    <h1>
+        Una lettera per te
+    </h1>
+
+    <button id="openLetter">
+        Apri ❤️
+    </button>
+
+</div>
+
+
+
+<div class="letter" id="letter">
+
+<p id="letterText"></p>
+
+</div>
+
+
+</div>
+
+
+<script src="script.js"></script>
+
+
+</body>
+
+</html>
