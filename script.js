5401e3e42cdf4d408f925bdab62e59e0a76262f7
Fra <jojoestarsupremacy@gmail.com>
2026-07-17 06:39:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -79,3 +79,65 @@ button.addEventListener("click", () => {
 
 
 });
+
+const openLetter = document.getElementById("openLetter");
+
+const envelope = document.getElementById("envelope");
+
+const letter = document.getElementById("letter");
+
+const letterText = document.getElementById("letterText");
+
+
+const finalLetter = 
+`Anni fa mi chiedevo cosa significasse davvero vivere la vita dei sogni e, soprattutto, come si facesse a capire quando finalmente ci sei arrivato.
+
+Oggi posso rispondere.
+
+Per la prima volta nella mia vita non desidero più nulla, perché sento di aver già guadagnato tutto.
+
+L'estate, la moto... ma soprattutto tu.
+
+Sei la cosa per cui ho lottato di più, quella per cui non ho mollato per anni.
+
+E alla fine ce l'ho fatta.
+
+Grazie a te sto vivendo la vita dei miei sogni, perché la verità è che eri tu il mio sogno.
+
+Sei riuscita a rendermi felice in un modo che non avrei mai immaginato.
+
+Ti amo ❤️`;
+
+
+
+openLetter.addEventListener("click", () => {
+
+
+    envelope.classList.add("hide-envelope");
+
+
+    letter.classList.add("open");
+
+
+    let i = 0;
+
+
+    function writeLetter(){
+
+        if(i < finalLetter.length){
+
+            letterText.innerHTML += finalLetter.charAt(i);
+
+            i++;
+
+            setTimeout(writeLetter,40);
+
+        }
+
+    }
+
+
+    writeLetter();
+
+
+});
