a1dc887251d68a8556c84305ab8bedf3814d60bf
Fra <jojoestarsupremacy@gmail.com>
2026-07-17 10:32:58 +0200

    Update lettera.js

--- lettera.js
+++ lettera.js
@@ -46,3 +46,33 @@ openLetter.addEventListener("click", () => {
     writeLetter();
 
 });
+
+function createHeart(){
+
+    const heart = document.createElement("div");
+
+    heart.classList.add("heart");
+
+    heart.innerHTML = "❤️";
+
+
+    heart.style.left = Math.random() * 100 + "vw";
+
+    heart.style.animationDuration =
+    (Math.random() * 3 + 3) + "s";
+
+
+    document.getElementById("hearts").appendChild(heart);
+
+
+    setTimeout(() => {
+
+        heart.remove();
+
+    },6000);
+
+}
+
+
+
+setInterval(createHeart,300);
