480ce20a18ed7a0b312f2cc28a56a60bcff34248
Fra <jojoestarsupremacy@gmail.com>
2026-07-17 10:06:39 +0200

    Create ricordi.js

---
+++ ricordi.js
@@ -0,0 +1,32 @@
+const memories = document.querySelectorAll(".memory");
+
+
+const observer = new IntersectionObserver((entries) => {
+
+
+    entries.forEach(entry => {
+
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+
+    });
+
+
+}, {
+
+    threshold:0.2
+
+});
+
+
+
+memories.forEach(memory => {
+
+    observer.observe(memory);
+
+});
